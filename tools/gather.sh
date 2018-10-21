e048a9096d2971cea96f1c6c94900c8a5ac702f6
jrbeverly <jrbeverly@live.com>
2018-10-20 21:59:25 -0400

    Update the brief template

--- tools/gather.sh
+++ tools/gather.sh
@@ -6,16 +6,20 @@ DIR_ROOT="$(dirname $DIR)"
 DIR_SRC="${DIR_ROOT}/src"
 DIR_REF="${DIR_SRC}/ref"
 DIR_BRIEF="${DIR_SRC}/brief"
-DIR_ARTIFACTS="${DIR_ROOT}/.artifacts/"
+DIR_TMP="${DIR_ROOT}/.artifacts/"
+DIR_ARTIFACTS="${DIR_ROOT}/dist/"
 DIR_ARTIFACTS_REF="${DIR_ARTIFACTS}/ref/"
 DIR_ARTIFACTS_BRIEF="${DIR_ARTIFACTS}/brief"
+DIR_ARTIFACTS_GIT="${DIR_ARTIFACTS}/tmp"
 
 FILE_REPO="${DIR}/REPOSITORIES"
 
+rm -rf $DIR_TMP
 rm -rf $DIR_ARTIFACTS
 mkdir -p $DIR_ARTIFACTS
 mkdir -p $DIR_ARTIFACTS_REF
 mkdir -p $DIR_ARTIFACTS_BRIEF
+mkdir -p $DIR_TMP
 
 while IFS="" read -r repo || [ -n "$repo" ]
 do
@@ -52,10 +56,17 @@ do
             git_url=$(echo $repo| tr : /| cut -d'@' -f 2)
             url="https://${git_url%.git}"
 
-            ## Briefs for projects
-            
-            cp -r "${DIR_BRIEF}/." .
+            ## Get the properties from repository
+            gitPath="$DIR_TMP/$dirname"
+            git clone $repo "$gitPath"
+            iconPath="docs/icon/icon.svg"
 
+            ## Briefs for projects           
+            cp -r "${DIR_BRIEF}/." .
+            mkdir -p "images/"
+            cp "$gitPath/$iconPath" images/icon.svg
+            cp "$gitPath/README.md" README.md
+   
             docker run -v $(pwd):/source jagregory/pandoc -f markdown -t html5 README.md -o output.html
 
             sed -i "s,%URL%,${url},g" index.html
